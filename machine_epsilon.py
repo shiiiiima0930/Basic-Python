a5e14794af644679b172e2a95ca92351bac8bcd2
志真 <shima.0930@icloud.com>
2023-07-01 12:04:52 +0900

    制御構文の解答

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,1 +1,11 @@
 # TODO
+epsilon = 1
+small = 1
+while True:
+    small *= 1/2
+    if 1 + epsilon > 1:
+        epsilon = small
+    else:
+        break
+
+print(epsilon)
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,19 @@
-from math import sin
+from math import sin, pi
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
+
+a = 0
+b = pi / 2
+N = 100
+
+S = 0
+h = (b - a) / N
+
+for k in range(N):
+    x1 = a + (k - 1) * h
+    x2 = a + k * h
+    S += (sin(x1) + sin(x2)) * h / 2
+
+print(S)
